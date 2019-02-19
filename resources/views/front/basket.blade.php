e63122096a8b7f2b4629ce066e33c44a0ede8313
RakAlex18 <novalex76@gmail.com>
2019-02-19 21:32:20 +0300

    оформление view basket

--- resources/views/front/basket.blade.php
+++ resources/views/front/basket.blade.php
@@ -3,46 +3,56 @@
 @section('content')
 
     @if(Session::has('cart'))
-
-        <table>
+        {{--  <td>
+              <a href="{{ route('reduce.by.one', $product['item']['id']) }}">Удалить</a>
+          </td>
+          <td>
+             <a href="{{ route('delete.product', $product['item']['id']) }}">Очистить</a>
+        </td>--}}
+        <table class="table table-hover table-dark">
+            <thead>
             <tr>
-                <td>№</td>
-                <td>Название</td>
-                <td>Количество</td>
-                <td>Цена за ед</td>
-                <td>Цена общая за продукт</td>
-                <td>+</td>
-                <td>- одной штуки</td>
-                <td>- всей позиции</td>
+                <th scope="col">#</th>
+                <th scope="col">Название</th>
+                <th scope="col">Количество</th>
+                <th scope="col">Цена за ед.товара</th>
+                <th scope="col">Цена общая за продукт</th>
+                <th scope="col">+добавить 1 ед</th>
+                <th scope="col">-удалить 1 ед</th>
+                <th scope="col">-Очистить всю строку</th>
             </tr>
-            @foreach($products as $product)
-                <tr>
-                    <td>1</td>
+            </thead>
+            <tbody>
+            <tr>
+                <?php $i = 1?>
+                @foreach($products as $product)
+                    <th scope="row"><?=$i?></th>
                     <td>{{ $product['item']['name'] }}</td>
                     <td>{{ $product['qty'] }}</td>
                     <td>{{ $product['item']['price'] }}</td>
                     <td>{{ $product['price'] }}</td>
-                    <td></td>
-                  {{--  <td>
-                        <a href="{{ route('reduce.by.one', $product['item']['id']) }}">Удалить</a>
+                    <td>
+                        <button type="button" class="btn btn-danger">добавить</button>
                     </td>
                     <td>
-                        <a href="{{ route('delete.product', $product['item']['id']) }}">Очистить</a>
-                    </td>--}}
-                </tr>
+                        <button type="button" class="btn btn-primary">удалить</button>
+                    </td>
+                    <td>
+                        <button type="button" class="btn btn-light">очистить</button>
+                    </td>
+            </tr>
+            <?php $i++?>
             @endforeach
+            </tbody>
         </table>
         <hr>
         <h2>Общее количество {{ $totalQty }}</h2>
         <h2>Общая цена {{ $totalPrice }}</h2>
-
-      {{--  <a href="{{ route('destroy') }}">Очистить корзину</a>--}}
+        {{--  <a href="{{ route('destroy') }}">Очистить корзину</a>--}}
+        <button type="button" class="btn btn-secondary">Очистить корзину</button>
+        <button type="button" class="btn btn-success">Оформить заказ</button>
 
     @else
-
         <p>Продуктов нет в корзине</p>
-
-        @endif
-
-
+    @endif
 @endsection
